5aa25fac7b98a9a0fad0fbab9fb0beb57f7282df
yookm19 <yookm19@gmail.com>
2018-10-30 20:44:31 +0900

    完了

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+							
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+// LINE Messaging APIがリクエストに付与した署名を取得
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader ::LINE_SIGNATURE];
+							
+// 署名が正当かチェック。正当であればリクエストをパースし配列へ
+// 不正であれば例外の内容を出力
+try {
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) { 
+  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+}
+
+$midFile = __DIR__ . "/files/mids";
+
+// midsの中身を読み込み
+$mids = explode(PHP_EOL, trim(file_get_contents($midFile)));
+
+// メッセージを送ってきたユーザーを取得
+$json = json_decode(file_get_contents("php://input"),true);
+$newMids = array();
+
+// 新規ユーザーの場合は追加
+$mids = array_merge($newMids, $mids);
+$mids = array_unique($mids);
+file_put_contents($midFile, implode(PHP_EOL, $mids));
+
+
+// 配列に格納された各イベントをループで処理
+foreach ($events as $event) {
+
+  // MessageEventクラスのインスタンスでなければ処理をスキップ
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+    error_log('Non message event has come');
+    continue;
+  }
+    
+  // MessageEventクラスのインスタンスでなければ処理をスキップ
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+    error_log('Non Text message event has come');
+    continue;
+  }
+
+  replyTextMessage($bot, $event->getReplyToken(), $event->getText());
+
+  // メッセージを全登録ユーザーID宛にプッシュ
+  // foreach ($mids as $mid) {
+  //   echo $mids;
+  //   echo $mid;
+  //   // テキストを返信し次のイベントの処理へ
+	//   pushTextMessage(array_values($mid), $event->getText());
+    
+  // }
+
+}
+
+
+// テキストを返信。引数はLINEBot、返信先、テキスト
+function replyTextMessage($bot, $replyToken, $text) {
+	// 返信を行いレスポンスを取得
+	// TextMessageBuilderの引数はテキスト
+	$response = $bot->replymessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+
+	// レスポンスが異常な場合
+	if(!$response->isSucceeded()) {
+		//エラー内容を出力
+		error_log('Failed! '. $response->getHTTPStatus . ' '. $response->getRawBody());
+	}
+}
+
+// テキストをプッシュ。引数はLINEBot、返信先、テキスト
+function pushTextMessage($bot, $userId, $text) {
+	// メッセージのプッシュを行いレスポンスを取得
+	// TextMessageBuilderの引数はテキスト
+	$response = $bot->pushMessage($userId, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+
+	// レスポンスが異常な場合
+	if(!$response->isSucceeded()) {
+		//エラー内容を出力
+		error_log('Failed! '. $response->getHTTPStatus . ' '. $response->getRawBody());
+	}
+}
+
+?>
---
+++ pushMessage.php
@@ -0,0 +1,52 @@
+<?php
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+							
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+$userId = 'Udeadbeefdeadbeefdeadbeefdeadbeef';
+
+$response = $bot->pushMessage('Udeadbeefdeadbeefdeadbeefdeadbeef', new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("Hello World!"));
+if(!$response->isSucceeded()) {
+	//エラー内容を出力
+	error_log('Failed! '. $response->getHTTPStatus . ' '. $response->getRawBody());
+}
+
+
+// ---
+// $mes = 'test';
+//To 
+// $to = 'Udeadbeefdeadbeefdeadbeefdeadbeef'; 
+//Channel_Access_Token 
+// $Channel_Access_Token = getenv('CHANNEL_ACCESS_TOKEN');
+// $response_format = [ 
+// 	"type" => "text", 
+// 	"text" => $mes 
+// 	];
+	
+// 	$post_data = [ 
+// 	"to" => $to, 
+// 	"messages" => [$response_format] 
+// 	];
+	
+// 	//post 
+// 	$channel = curl_init("https://api.line.me/v2/bot/message/push"); 
+// 	curl_setopt($channel, CURLOPT_POST, true); 
+// 	curl_setopt($channel, CURLOPT_CUSTOMREQUEST, 'POST'); 
+// 	curl_setopt($channel, CURLOPT_RETURNTRANSFER, true); 
+// 	curl_setopt($channel, CURLOPT_POSTFIELDS, json_encode($post_data)); 
+// 	curl_setopt($channel, CURLOPT_HTTPHEADER, array( 
+// 	'Content-Type: application/json; charset=UTF-8', 
+// 	'Authorization: Bearer ' . $Channel_Access_Token 
+// 	));
+	
+// 	$result = curl_exec($channel); 
+// 	// var_dump($result); 
+// 	curl_close($channel); 
+
+
+?>
